8b9b885a990c4fc6b9234f69646197c49fd08b18
elvin <elvinalishov84@gmail.com>
2020-09-03 12:51:41 +0400

    fix

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -7,7 +7,15 @@
       <hr class="contact-block__line-decor__bold" />
     </div>
     <div class="contact-block__content">
-      
+      <div class="contact-block__content__message">
+        <h2 class="contact-block__content__message__title">Get in Touch</h2>
+        <input type="text" class="contact-block__content__message__input" placeholder="First name">
+        <input type="text" class="contact-block__content__message__input" placeholder="Last name">
+        <input type="text" class="contact-block__content__message__input" placeholder="Mobile number">
+        <input type="text" class="contact-block__content__message__input" placeholder="Email">
+        <textarea name="" id="" cols="30" rows="8" placeholder="Messages" class="contact-block__content__message__input" style="resize: none;"></textarea>
+        <button class="contact-block__content__message__button">Submit</button>
+      </div>
     </div>
   </div>
 </template>
